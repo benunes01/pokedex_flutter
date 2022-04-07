51d301b5b2a75fd4431f313e9c8898772f6c1f64
Bernardo Nunes <39046776+benunes01@users.noreply.github.com>
2022-04-07 03:50:10 -0300

    feaT: Usando o Slidy para gerar as pastas modulares

---
+++ lib/app/modules/home/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'home_store.dart';
+
+class HomePage extends StatefulWidget {
+  final String title;
+  const HomePage({Key? key, this.title = "Home"}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends ModularState<HomePage, HomeStore> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter'),
+      ),
+      body: Observer(
+        builder: (context) => Container(
+            alignment: Alignment.center,
+            child: Text('${store.counter}', style: const TextStyle(fontSize: 50, color: Colors.blue))
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          store.increment();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
